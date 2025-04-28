62686bb6fa9f0f2971d00d2eb079fbf75adeb673
YanGraim <yangraim@gmail.com>
2025-04-28 16:20:06 -0300

    cria rotas dinamicas

---
+++ src/app/repositorios/[id]/page.tsx
@@ -0,0 +1,20 @@
+interface PageDetailProps {
+  params: {
+    id: string;
+  };
+}
+
+async function getData(id: string) {
+  console.log(id);
+  const response = await fetch("https://api.github.com/users/yangraim/repos");
+  return response.json();
+}
+
+export default async function RepositorioId({ params }: PageDetailProps) {
+  const data = await getData(params.id);
+  return (
+    <div>
+      <h1>Pagina detalhes do repositorio {params.id}</h1>
+    </div>
+  );
+}
