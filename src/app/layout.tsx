58c7ea5b6a7e014d0d9ba47b49a11240455d9754
YanGraim <yangraim@gmail.com>
2025-04-29 14:57:11 -0300

    utiliza metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import { Header } from "@/components/header";
 export const metadata: Metadata = {
   title: "Meu Site - Aprendendo NextJS",
   description: "Aprendendo NextJS",
+  keywords: ["HTML", "CSS", "JavaScript", "Programação"],
+  openGraph: {
+    images: [
+      /*colar url da imagem, thumb do link*/
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
 };
 
 export default function RootLayout({
